3190f441b36e8220ccd8d392e1ca32505a098eb7
Drachier <r.milbradt@tum.de>
2024-11-29 15:23:19 +0100

    Implement recursive compression

--- pytenet/compression.py
+++ pytenet/compression.py
@@ -2,12 +2,12 @@
 Implements Compression algorithms for MPS
 """
 
-__all__ = ['rounding']
+__all__ = ['rounding', 'recursive']
 
 from numpy import array, tensordot, zeros_like
 from numpy.linalg import eigh
 
-from .bond_ops import eigh
+from .bond_ops import eigh, split_matrix_svd
 from .operation import compute_left_state_blocks_conj
 from .mps import MPS
 from .qnumber import qnumber_flatten
@@ -22,7 +22,7 @@ def rounding(mps: MPS, tol: float) -> MPS:
     nsites = mps.nsites
     left_envs = compute_left_state_blocks_conj(mps)
     new_mps_tensors = [None for _ in range(nsites)]
-    new_virtual_qnums = [zeros_like(qDs) for qDs in mps.qD]
+    new_virtual_qnums = [None for qDs in mps.qD]
     new_virtual_qnums[-1] = mps.qD[-1]
     # Implement trivial initial tensors
     b_block = array([1], dtype=complex).reshape(1,1,1)
@@ -63,6 +63,76 @@ def rounding(mps: MPS, tol: float) -> MPS:
                         axes=([2], [0]))
     new_mps_tensors[0] = b_block
     new_virtual_qnums[0] = mps.qD[0]
+    # Create compressed mps
+    new_mps = MPS(mps.qd, new_virtual_qnums, fill='postpone')
+    new_mps.A = new_mps_tensors
+    return new_mps
+
+def recursive(mps: MPS, tol: float, orth: str | None ='left') -> MPS:
+    """
+    Implements the recursive compression algorithm.
+
+    Args:
+        mps: The MPS to be compressed.
+        tol: The tolerance for the compression.
+        orth: The orthogonality center of the MPS. If None, the MPS will
+            be left-orthonormalized and compressed accordingly.
+
+    """
+    if orth is None or orth == 'none':
+        mps.orthonormalize()
+        return recursive(mps, tol, orth='left')
+    nsites = mps.nsites
+    new_mps_tensors = [None for _ in range(nsites)]
+    new_virtual_qnums = [None for _ in mps.qD]
+    current_projector = array([1], dtype=complex).reshape(1,1)
+    if orth == 'left':
+        new_virtual_qnums[-1] = mps.qD[-1]
+        for site in reversed(range(nsites)):
+            # Contract projector to the right
+            site_tensor = tensordot(mps.A[site],
+                                    current_projector,
+                                    axes=([2], [0]))
+            if site != 0:
+                # Find new projector
+                site_matrix = site_tensor.transpose([1,0,2])
+                site_matrix = site_matrix.reshape(site_matrix.shape[0], -1)
+                q0 = mps.qD[site]
+                q1 = qnumber_flatten([-1*mps.qd, new_virtual_qnums[site+1]])
+                current_projector, _, _, new_qn = split_matrix_svd(site_matrix,q0,q1,tol)
+                # Contract projector to the left
+                site_tensor = tensordot(site_tensor,
+                                        current_projector.conj(),
+                                        axes=([1], [0]))
+                site_tensor = site_tensor.transpose([0,2,1])
+            else:
+                # The leftmost site is already of bond dimension 1
+                new_qn = mps.qD[0]
+            new_mps_tensors[site] = site_tensor
+            new_virtual_qnums[site] = new_qn
+    elif orth == 'right':
+        new_virtual_qnums[0] = mps.qD[0]
+        for site in range(nsites):
+            # Contract projector to the left
+            site_tensor = tensordot(mps.A[site],
+                                    current_projector,
+                                    axes=([1], [1]))
+            site_tensor = site_tensor.transpose([0,2,1])
+            if site != nsites-1:
+                # Find new projector
+                site_matrix = site_tensor.reshape(-1, site_tensor.shape[2])
+                q0 = qnumber_flatten([mps.qd, new_virtual_qnums[site]])
+                q1 = mps.qD[site+1]
+                _, _, current_projector, new_qn = split_matrix_svd(site_matrix,q0,q1,tol)
+                # Contract projector to the right
+                site_tensor = tensordot(site_tensor,
+                                        current_projector,
+                                        axes=([2], [1]))
+            else:
+                # The rightmost site is already of bond dimension 1
+                new_qn = mps.qD[-1]
+            new_mps_tensors[site] = site_tensor
+            new_virtual_qnums[site] = new_qn
     # Create compressed mps
     new_mps = MPS(mps.qd, new_virtual_qnums, fill='postpone')
     new_mps.A = new_mps_tensors
--- test/test_compression.py
+++ test/test_compression.py
@@ -3,7 +3,7 @@ import unittest
 from numpy import allclose
 from numpy.random import default_rng
 
-from pytenet.compression import rounding
+from pytenet.compression import rounding, recursive
 from pytenet.mps import MPS
 
 class TestRoundingCompression(unittest.TestCase):
@@ -34,7 +34,45 @@ class TestRoundingCompression(unittest.TestCase):
 
         compr_mps = rounding(mps, tol=0.15)
 
+        # Ensure that truncation actually happened
+        self.assertTrue(sum(compr_mps.bond_dims) < sum(mps.bond_dims))
+
         self.assertTrue(allclose(mps.as_vector(), compr_mps.as_vector(), rtol=0.15))
+
+class TestRecursiveCompression(unittest.TestCase):
+
+    def test_no_truncation(self):
+        # Create a random MPS
+        rng = default_rng(seed=12344623452)
+
+        phys_dim = 3
+        virt_dims = [1, 3, 5, 7, 3, 1]
+        qd = rng.integers(-2, 3, size=phys_dim)
+        qD = [rng.integers(-2, 3, size=Di) for Di in virt_dims]
+        mps = MPS(qd, qD, fill='random', rng=rng)
+        mps.orthonormalize()
+
+        compr_mps = recursive(mps, tol=-1)
+
+        self.assertTrue(allclose(mps.as_vector(), compr_mps.as_vector()))
+
+    def test_truncation(self):
+        # Create a random MPS
+        rng = default_rng(seed=12344623452)
+
+        phys_dim = 3
+        virt_dims = [1, 3, 5, 7, 3, 1]
+        qd = rng.integers(-2, 3, size=phys_dim)
+        qD = [rng.integers(-2, 3, size=Di) for Di in virt_dims]
+        mps = MPS(qd, qD, fill='random', rng=rng)
+        mps.orthonormalize()
+
+        compr_mps = recursive(mps, tol=0.01)
+
+        # Ensure that truncation actually happened
+        self.assertTrue(sum(compr_mps.bond_dims) < sum(mps.bond_dims))
+
+        self.assertTrue(allclose(compr_mps.as_vector(), mps.as_vector(), rtol=0.1))
 
 if __name__ == '__main__':
     unittest.main()
